739a967f0653bb4fbdb36dde2f0ee2f5e59e3a48
jahinbhuiyan <jahinbarobhuiyan@gmail.com>
2023-03-05 20:20:14 -0500

    palindrome Edit

--- solutions.js
+++ solutions.js
@@ -34,3 +34,13 @@ var twoSum = function(nums, target) {
     
 };
 
+
+//  x < 0  is checking if the number is negative; negative numbers can't be palindrome;
+// x%10 is checking whether the number is a multiple of 10; if a number is a multiple of 10
+// the last digit is 0; any number with last digit 0 can't be palindrome; for example":
+// 150 in reverse is 51;
+// finally we are checking if x!= 0
+
+
+
+
