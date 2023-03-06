d2d91d80413bac345935db835428e75182c5bac5
jahinbhuiyan <jahinbarobhuiyan@gmail.com>
2023-03-05 19:50:36 -0500

    twoSum

--- solutions.js
+++ solutions.js
@@ -1,12 +1,16 @@
 // twoSum
 
-let obj = {}
-for (i=0;i<nums.length;i++){
-    let found = target-nums[i]
-    if(obj[found] != null){
-        return [i,obj[found]]
-    }else{
-        obj[nums[i]] = i
+var twoSum = function(nums, target) {
+    let obj = {};
+    for(let i =0 ; i < nums.length; i++){
+        let found = target - nums[i];
+        if(obj[found] !== undefined){
+            return [i, obj[found]];
+        }
+        else{
+            obj[nums[i]] = i;
+        }
+ 
     }
-}
+ };
 
